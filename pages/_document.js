ae1dd4eb0bc4613047b6f1b592b526e35959122c
Royalcoster <cobol1962@gmail.com>
2022-08-08 19:00:25 +0200

    6

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,6 @@ export default class MyDocument extends Document {
             <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
             <link rel="stylesheet" href="css/sweetalert.css" />
             <link rel="stylesheet" href="css/typeahead.css" type="text/css"/>
-            <!--<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.16/b-1.5.1/b-colvis-1.5.1/b-flash-1.5.1/b-html5-1.5.1/b-print-1.5.1/cr-1.4.1/fc-3.2.4/fh-3.1.3/sc-1.4.4/sl-1.2.5/datatables.min.css"/>-->
             <link rel="stylesheet" href="css/main.css?v=<?=time()?>" type="text/css"/>
             <link rel="stylesheet" href="css/croppie.css" type="text/css"/>
             <link rel="stylesheet" href="css/dropzone.css" type="text/css"/>

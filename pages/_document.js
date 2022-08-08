0a19d8cca2ba34dc1efed20508b99c268280858c
Royalcoster <cobol1962@gmail.com>
2022-08-08 18:53:05 +0200

    2

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+<link rel="stylesheet" href="css/sweetalert.css" />
+<link rel="stylesheet" href="css/typeahead.css" type="text/css"/>
+<!--<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.16/b-1.5.1/b-colvis-1.5.1/b-flash-1.5.1/b-html5-1.5.1/b-print-1.5.1/cr-1.4.1/fc-3.2.4/fh-3.1.3/sc-1.4.4/sl-1.2.5/datatables.min.css"/>-->
+<link rel="stylesheet" href="css/main.css?v=<?=time()?>" type="text/css"/>
+<link rel="stylesheet" href="css/croppie.css" type="text/css"/>
+<link rel="stylesheet" href="css/dropzone.css" type="text/css"/>
+<link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+<link href="css/gallery-dark-materialize.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="css/social.css" type="text/css"/>
+<link rel="stylesheet" href="css/select2.css" type="text/css"/>
+<link rel="stylesheet" href="css/justifiedGallery.min.css" type="text/css"/>
+<link rel="stylesheet" href="css/lightgallery.css" type="text/css"/>
+<link rel="stylesheet" href="css/all.css" />

c69cfb5f7c8c2397154628c65ee8aad77e5fdd85
Royalcoster <cobol1962@gmail.com>
2022-08-08 18:46:35 +0200

    1

--- next.config.js
+++ next.config.js
@@ -5,3 +5,17 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+const path = require('path')
+
+module.exports = {
+  reactStrictMode: true,
+}
+module.exports = {
+
+  assetPrefix: 'https://goat100.com/',
+  images: {
+    minimumCacheTTL: 86400,
+    domains: ["goat100.com"]
+  },
+
+}
